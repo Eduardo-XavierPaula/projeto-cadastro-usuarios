fbc18fd184266f76b690f2ddea16e7b5bf269bc1
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-07-27 16:06:36 -0300

    ✨ New feature: Criação de services, interfaces e types do projeto, by Eduardo Xavier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
+import { GenresService } from './services/genres.service';
+import { BrazilianStatesService } from './services/brazilian-states.service';
+import { UsersPlaceholderService } from './services/users-placeholder.service';
+import { UsersListResponse } from './types/users-list-response';
+import { GenresListResponse } from './types/genres-list-response';
+import { StatesListResponse } from './types/states-list-response';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'projeto-cadastro-usuarios';
+export class AppComponent implements OnInit {
+  usersList: UsersListResponse = [];
+  genresList: GenresListResponse = [];
+  statesList: StatesListResponse = [];
+
+  constructor(
+    private readonly _usersService: UsersService,
+    private readonly _genresService: GenresService,
+    private readonly _statesService: BrazilianStatesService
+  ) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+    this.getGenres();
+    this.getStates();
+  }
+
+  private getUsers() {
+    this._usersService.getUsers().subscribe((usersListResponse) => {
+      this.usersList = usersListResponse;
+      console.log(usersListResponse);
+    });
+  }
+
+  private getGenres() {
+    this._genresService.getGenres().subscribe((genresListResponse) => {
+      this.genresList = genresListResponse;
+      console.log(this.genresList);
+    });
+  }
+
+  private getStates() {
+    this._statesService.getStates().subscribe((statesListResponse) => {
+      this.statesList = statesListResponse;
+      console.log(this.statesList);
+    });
+  }
 }
