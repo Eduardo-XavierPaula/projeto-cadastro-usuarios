ae4830803aca95577aba6e49c3f7c4e31307bfa9
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-08-11 16:01:43 -0300

    ✨ New feature: Finalizando campo de data, by Eduardo Xavier

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -9,6 +9,9 @@ import { GenresListResponse } from '../../types/genres-list-response';
 import { StatesListResponse } from '../../types/states-list-response';
 import { IUser } from '../../interfaces/user/user.interface';
 import { getPasswordStrengthValue } from '../../utils/get-password-strength-value';
+import { convertPtBrDateToDateObj } from '../../utils/convert-pt-br-date-to-date-obj';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { convertDateObjToPtBrDate } from '../../utils/convert-date-obj-to-pt-br-date';
 
 @Component({
   selector: 'app-user-form',
@@ -17,6 +20,8 @@ import { getPasswordStrengthValue } from '../../utils/get-password-strength-valu
 })
 export class UserFormComponent implements OnInit, OnChanges {
   passwordStrengthValue = 0;
+
+  dateValue: Date | null = null;
 
   minDate: Date | null = null;
   maxDate: Date | null = null;
@@ -29,19 +34,33 @@ export class UserFormComponent implements OnInit, OnChanges {
     this.setMinAndMaxDate();
   }
 
-  private setMinAndMaxDate() {
-    this.minDate = new Date(new Date().getFullYear() - 100, 0, 1);
-    this.maxDate = new Date();
-  }
-
   ngOnChanges(changes: SimpleChanges): void {
     const USER_CHANGED = changes['userSelected'];
     if (USER_CHANGED) {
       this.onPasswordChange(this.userSelected.password);
+      this.setBirthDateToDatepicker(this.userSelected.birthDate);
     }
   }
 
   onPasswordChange(password: string) {
     this.passwordStrengthValue = getPasswordStrengthValue(password);
+  }
+
+  onDateChange(event: MatDatepickerInputEvent<any, any>) {
+    if (!event.value) {
+      return;
+    }
+
+    this.userSelected.birthDate = convertDateObjToPtBrDate(event.value);
+    console.log(this.userSelected.birthDate);
+  }
+
+  private setMinAndMaxDate() {
+    this.minDate = new Date(new Date().getFullYear() - 100, 0, 1);
+    this.maxDate = new Date();
+  }
+
+  private setBirthDateToDatepicker(birthDate: string) {
+    this.dateValue = convertPtBrDateToDateObj(birthDate);
   }
 }
